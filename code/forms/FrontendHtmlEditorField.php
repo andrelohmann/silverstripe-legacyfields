61768650b2fbe58b103322b9e22c79e7b9ac325c
helpfulrobot <chris@silverstripe.com>
2015-12-18 14:07:21 +1300

    Converted to PSR-2

--- code/forms/FrontendHtmlEditorField.php
+++ code/forms/FrontendHtmlEditorField.php
@@ -1,8 +1,10 @@
 <?php
 
-class FrontendHtmlEditorField extends HtmlEditorField {
+class FrontendHtmlEditorField extends HtmlEditorField
+{
 
-    public function __construct($name, $title = null, $value = '') {
+    public function __construct($name, $title = null, $value = '')
+    {
         parent::__construct($name, $title, $value);
         HtmlEditorConfig::require_js();
         Requirements::customScript("ssTinyMceConfig.mode = 'specific_textareas';ssTinyMceConfig.editor_selector='frontendhtmleditor';tinyMCE.init(ssTinyMceConfig);");
